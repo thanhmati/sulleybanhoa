ee664bce1be528a7e3d8e7cd764318744eaffb3f
thanhmati <thanh.do@mati.com.vn>
2025-10-13 11:10:07 +0700

    feat/#: use env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3030/api/v1',
+  baseURL: import.meta.env.VITE_BACKEND_DOMAIN,
   headers: {
     'Content-Type': 'application/json',
   },
